a74f96a0a0e7f5eaf894158ba1f218b99135de61
Betsy Kalkwarf <109998791+Betsy-Kalkwarf@users.noreply.github.com>
2022-10-16 08:02:48 -0700

    Add files via upload
    More js added

--- static/js/app.js
+++ static/js/app.js
@@ -22,3 +22,22 @@ data.forEach((dataRow) => {
 
 }
 
+//function for when button to sort by date is clicked
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    if (date){
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+
+    buildTable(filteredData);
+}
+
+d3.selectAll("#filter-btn").on("click", handleClick)
+
+//builds table when page loads
+buildTable(tableData);
+
+
+
